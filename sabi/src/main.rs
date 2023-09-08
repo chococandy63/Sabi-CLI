aaf134663883b46c749b22bb3b5d67d647cb4bc2
Riya Bisht <manasi.riya2003@gmail.com>
2023-09-09 00:14:15 +0530

    add code

--- sabi/src/main.rs
+++ sabi/src/main.rs
@@ -14,6 +14,17 @@ fn main() {
     let args = Cli::parse();
     println!("Arguement 1 - {}",args.pattern);
     println!("Arguement 2 - {}",args.path.display());
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+    
+
+
 }
 
 
