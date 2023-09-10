8fd5ab526576810b1613cf5fbf741abfed4d567d
Riya Bisht <manasi.riya2003@gmail.com>
2023-09-10 16:16:43 +0530

    edit code

--- sabi/src/main.rs
+++ sabi/src/main.rs
@@ -12,6 +12,24 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
+
+
 fn main() -> Result<()> {
     let args = Cli::parse();
 println!("Arguement 1 - {}",args.pattern);
@@ -21,23 +39,18 @@ println!("Arguement 2 - {}",args.path.display());
     let content = std::fs::read_to_string(path)
         .with_context(|| format!("could not read file `{}`", path))?;
     println!("file content: {}", content);
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
     
         let pb = indicatif::ProgressBar::new(100);
         for i in 0..100 {
-            //do_hard_work();
+            find_matches(&content, &args.pattern, &mut std::io::stdout());
             pb.println(format!("[+] finished #{}", i));
             pb.inc(1);
         }
         pb.finish_with_message("done");
     
-        env_logger::init();
-        info!("starting up");
-        warn!("oops, nothing implemented!");
+        // env_logger::init();
+        // info!("starting up");
+        // warn!("oops, nothing implemented!");
 
     Ok(())
 }
